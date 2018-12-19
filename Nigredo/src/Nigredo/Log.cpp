14847f0b3f6a67eb44c45c2e6d89d0a7c0bac527
Maximilian Kruse <maxkruse1996@gmail.com>
2018-12-19 23:07:14 +0100

    Cleanup of old file-logging code

--- Nigredo/src/Nigredo/Log.cpp
+++ Nigredo/src/Nigredo/Log.cpp
@@ -11,33 +11,13 @@ namespace Nigredo {
 	{
 		spdlog::set_pattern("%^[%H:%M:%S:%e] %n: %v%$");
 				
-		if (CreateDirectory(L"../logs/", NULL) ||
-			ERROR_ALREADY_EXISTS == GetLastError())
-		{
-			
-		}
-		else
-		{
-			throw "Couldnt create folder \"../logs/\"";
-		}
-
 		char buffer[512];
 		struct tm newtime;
 		time_t now = time(0);
 		localtime_s(&newtime, &now);
 
-#ifdef NIGREDO_LOG_TO_FILE
-
-		strftime(buffer, sizeof(buffer), "..\\logs\\log_%Y_%m_%d - %H-%M-%S.txt", &newtime);
-
-		const spdlog::filename_t &str(buffer);
-
-		s_CoreLogger = spdlog::rotating_logger_mt("CORE", str, 1024*1024*256, 4);
-		s_CoreLogger->set_level(spdlog::level::trace);
-#else
 		s_CoreLogger = spdlog::stdout_color_mt("CORE");
 		s_CoreLogger->set_level(spdlog::level::trace);
-#endif
 
 		s_ClientLogger = spdlog::stdout_color_mt("CLIENT");
 		s_ClientLogger->set_level(spdlog::level::trace);
