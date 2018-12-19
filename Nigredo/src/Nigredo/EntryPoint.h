9d27c68d6bd78783fddd334dd06cfcfafbccf1bc
Maximilian Kruse <maxkruse1996@gmail.com>
2018-12-19 23:18:36 +0100

    Dont use Logger before initializing it

--- Nigredo/src/Nigredo/EntryPoint.h
+++ Nigredo/src/Nigredo/EntryPoint.h
@@ -6,17 +6,16 @@ extern Nigredo::Application* Nigredo::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	NIGREDO_CRITICAL_CORE("Welcome to Albedo (Sandbox), Nigredo (Core), Rubedo (Unused)!");
-
 	Nigredo::Log::Init();
 
+	NIGREDO_WARN_CORE("Welcome to Albedo (Sandbox), Nigredo (Core), Rubedo (Unused)!");
 	NIGREDO_WARN_CORE("!!RUNNING IN DEBUG MODE!!");
 	NIGREDO_INFO_CORE("Initialized");
 
 
 	auto app = Nigredo::CreateApplication();
 	app->Run();
-	NIGREDO_CRITICAL_CORE("WE SHUT DOWN");
+	NIGREDO_WARN_CORE("WE SHUT DOWN");
 	delete app;
 }
 
